eef6c6164732c087baeaeef33bc00e5a5fe2f4ff
Qelt <piterson.wojcik@gmail.com>
2022-10-15 18:52:06 +0200

    generowanie różnych platform

--- Assets/Cody/GenerowaniePlatform.cs
+++ Assets/Cody/GenerowaniePlatform.cs
@@ -7,7 +7,12 @@ public class GenerowaniePlatform : MonoBehaviour
     public static GenerowaniePlatform Instance;
 
     public GameObject Platforma;
+    public GameObject Platforma1;
+    public GameObject Platforma2;
+    public GameObject Platforma3;
     public float odstep = 20; 
+
+    private int losowaniePlatfomy;
 
     void Awake()
     {
@@ -29,7 +34,35 @@ public class GenerowaniePlatform : MonoBehaviour
 
     public void UtworzPlatforme()
     {
-        Instantiate(Platforma, new Vector3(odstep, Random.Range(-6f, -2f), 0), Quaternion.Euler(0, 0, 0)) ;
+        losowaniePlatfomy = Random.Range( 1, 4);
+
+        if ( losowaniePlatfomy == 1)
+        {
+            UtworzPlatforme1();
+        } else if ( losowaniePlatfomy == 2)
+        {
+            UtworzPlatforme2();
+        } else if ( losowaniePlatfomy == 3)
+        {
+            UtworzPlatforme3();
+        }
+    }
+
+    public void UtworzPlatforme1()
+    {
+        Instantiate(Platforma1, new Vector3(odstep, Random.Range(-6f, -2f), 0), Quaternion.Euler(0, 0, 0)) ;
+        odstep += 12;
+    }
+
+    public void UtworzPlatforme2()
+    {
+        Instantiate(Platforma2, new Vector3(odstep - 4, Random.Range(-2f, 3f), 0), Quaternion.Euler(0, 0, 0)) ;
+        odstep += 14;
+    }
+
+    public void UtworzPlatforme3()
+    {
+        Instantiate(Platforma3, new Vector3(odstep + 5, Random.Range(-6f, -2f), 0), Quaternion.Euler(0, 0, 0)) ;
         odstep += 12;
     }
 }
